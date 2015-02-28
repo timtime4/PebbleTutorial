a6758c1a2dcc5f3ad7e6649928778c545b9ecebe
Tim Pusateri <tpusateri44@gmail.com>
2015-02-28 12:11:14 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -98,8 +98,8 @@ static void init() {
   // register callbacks
   app_message_register_inbox_received(inbox_received_callback);
   app_message_register_inbox_dropped(inbox_dropped_callback);
-  app_message_register_outbox_failed(outbox_failed_callback);
-//  app_message_register_outbox_sent(outbox_sent_callback);
+  //app_message_register_outbox_failed(outbox_failed_callback);
+ // app_message_register_outbox_sent(outbox_sent_callback);
 
   // Open message
   app_message_open(app_message_inbox_size_maximum(), app_message_inbox_size_maximum());
